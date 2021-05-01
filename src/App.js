d46e4807bbbf1fcb8291fb6139effdab70e79419
pnek <porysokheng@gmail.com>
2021-05-01 14:55:53 +0700

    Add title component to the page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Title from './components/Title';
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <Title />
     </div>
   );
 };
---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h3>Global</h3>
+      <p>Friday 24th 2021, 8:30pm</p>
+    </div>
+  );
+};
+
+export default Title;
