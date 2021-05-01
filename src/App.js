f4cba556fef31087502ade652a5177b1ff725782
pnek <porysokheng@gmail.com>
2021-05-01 16:40:54 +0700

    Add cards component to show the global data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 import Navbar from './components/Navbar';
 import Title from './components/Title';
+import Cards from './components/Cards';
 
-const App = () => {
-  const [globalDatas, setGlobalDatas] = useState({});
-
-  useEffect(() => {
-    getGlobal();
-  }, []);
-
-  const getGlobal = async () => {
-    const res = await axios.get('https://api.covid19api.com/summary');
-    setGlobalDatas(res.data.global);
+class App extends Component {
+  state = {
+    globalDatas: {},
   };
 
-  return (
-    <div className="App">
-      <Navbar />
-      <Title />
-    </div>
-  );
-};
+  componentDidMount() {
+    this.getGlobal();
+  }
+
+  getGlobal = async () => {
+    const res = await axios.get('https://api.covid19api.com/summary');
+    this.setState({ globalDatas: res.data.Global });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Title />
+        <Cards globalDatas={this.state.globalDatas} />
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/Cards.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Cards = (props) => {
+  const {
+    NewConfirmed,
+    NewDeaths,
+    TotalConfirmed,
+    TotalDeaths,
+    Date,
+  } = props.globalDatas;
+  return (
+    <div className="container">
+      <div className="cards">
+        <div className="card">
+          <h2>Cases</h2>
+          <div className="text-wrap">
+            <p>
+              New Case: <span>{NewConfirmed}</span>
+            </p>
+            <p>
+              Total Case: <span>{TotalConfirmed}</span>
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <h2>Deaths</h2>
+          <div className="text-wrap">
+            <p>
+              New Death: <span>{NewDeaths}</span>
+            </p>
+            <p>
+              Total Death: <span>{TotalDeaths}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
