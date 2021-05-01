8e1126227f12c0e849f24039d0966157beb87d3c
pnek <porysokheng@gmail.com>
2021-05-01 15:10:06 +0700

    Get global data from api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 import Navbar from './components/Navbar';
 import Title from './components/Title';
 
 const App = () => {
+  const [globalDatas, setGlobalDatas] = useState({});
+
+  useEffect(() => {
+    getGlobal();
+  }, []);
+
+  const getGlobal = async () => {
+    const res = await axios.get('https://api.covid19api.com/summary');
+    setGlobalDatas(res.data.global);
+  };
+
   return (
     <div className="App">
       <Navbar />
